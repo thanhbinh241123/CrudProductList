fea5f4919ba6b7673f2d8132730f6021dc92eeba
thanhbinh241123 <116929623+thanhbinh241123@users.noreply.github.com>
2024-06-29 09:49:25 +0700

    Add files via upload

---
+++ src/main/java/thidk.codelean.jdbc/Product.java
@@ -0,0 +1,59 @@
+package thidk.codelean.jdbc;
+
+public class Product {
+
+    private int id;
+    private String name;
+    private double price;
+    private String imageUrl;
+
+    public Product(String name, double price, String imageUrl) {
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public Product(int id, String name, double price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [id=" + id + ", name=" + name + ", price=" + price + ", imageUrl=" + imageUrl + "]";
+    }
+}
